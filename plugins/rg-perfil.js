ee6a0217040e5cc77066bf71e5599850699e330e
Wichi <sebastianbarbaro82@gmail.com>
2025-06-13 13:54:18 -0400

    Actualizar rg-perfil.js

--- plugins/rg-perfil.js
+++ plugins/rg-perfil.js
@@ -2,7 +2,7 @@ import PhoneNumber from 'awesome-phonenumber';
 import fetch from 'node-fetch';
 import fs from 'fs';
 
-const imagen1 = 'https://qu.ax/iVZTn.jpg';
+const imagen1 = 'https://files.catbox.moe/ltq7ph.jpg';
 
 const loadMarriages = () => {
     if (fs.existsSync('./media/database/marry.json')) {
