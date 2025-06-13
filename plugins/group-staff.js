6a52091266d51eac8b37aabd60736e665b4852be
Wichi <sebastianbarbaro82@gmail.com>
2025-06-13 13:52:37 -0400

    Actualizar group-staff.js

--- plugins/group-staff.js
+++ plugins/group-staff.js
@@ -26,7 +26,7 @@ let handler = async (m, { conn }) => {
 
   try {
     // Verificar variables globales con valores predeterminados
-    const imageUrl = global.imageUrl || "https://qu.ax/LJEVX.jpg"; // Imagen predeterminada
+    const imageUrl = global.imageUrl || "https://files.catbox.moe/ltq7ph.jpg"; // Imagen predeterminada
     const sourceUrl = global.redes || "https://github.com/Deylinel/TECNO-BOT-OFICIAL"; // URL del proyecto
     const thumbnailUrl = global.icono || "https://files.catbox.moe/owl2rl.jpg"; // Miniatura
 
