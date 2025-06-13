df3880b2dcaf26c3a78d2d75e73abfc2c52ec5fe
Wichi <sebastianbarbaro82@gmail.com>
2025-06-13 19:01:15 -0400

    Actualizar _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,28 +1,15 @@
-export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner, isMods }) {
 
-if (m.isBaileys && m.fromMe) return !0
-if (m.isGroup) return !1
-if (!m.message) return !0
+export async function before(m, { conn, isOwner, isROwner}) {
+    if (m.isBaileys && m.fromMe) return true;
+    if (m.isGroup) return false;
+    if (!m.message) return true;
 
-if (m.text.includes('serbot') || m.text.includes('code') || m.text.includes('--code') || m.text.includes('subbot') || m.text.includes('qr')) return !0
+    const botSettings = global.db.data.settings[this.user.jid] || {};
 
-const chat = global.db.data.chats[m.chat]
-const bot = global.db.data.settings[conn.user.jid] || {}
-
-const senderNumber = m.sender.split('@')[0]
-const forbiddenPrefixes = ["212", "265", "234", "258", "263", "93", "967", "92", "254", "213","505"]
-
-if (bot.antiPrivate && !isOwner && !isROwner && !isMods) {
-
-const isForbiddenPrefix = forbiddenPrefixes.some(prefix => senderNumber.startsWith(prefix))
-
-let reason = isForbiddenPrefix ? `Dejen De Joder 🥰.` : `Está Prohibido Escribir Al Privado Del Bot 🪐.`
-
-await m.reply(`> 《★》@${senderNumber} ${reason}\n\n- \`Si deseas probar el bot, sigue el canal oficial:\`\n> ${channel}`, false, { mentions: [m.sender] })
-await conn.updateBlockStatus(m.chat, 'block')
-
-return !0
+    if (botSettings.antiPrivate &&!isOwner &&!isROwner) {
+        await conn.updateBlockStatus(m.chat, 'block'); // Bloquea al usuario sin enviar mensaje
+        console.log(`Usuario ${m.sender} bloqueado por contacto privado.`);
 }
 
-return !1
+    return false;
 }
