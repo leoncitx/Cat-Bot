b9f37d1d54ab092c6b32c98b1a9819d0f1dfbdbb
Wichi <sebastianbarbaro82@gmail.com>
2025-06-13 19:02:18 -0400

    Actualizar _antiara.js

--- plugins/_antiara.js
+++ plugins/_antiara.js
@@ -1,16 +1,28 @@
-export async function before(m, { conn, isOwner, isROwner}) {
-    if (m.isBaileys && m.fromMe) return true;
-    if (m.isGroup) return false;
-    if (!m.message) return true;
+export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner, isMods }) {
 
-    const botSettings = global.db.data.settings[this.user.jid] || {};
-    const blockedPrefixes = ['+212']; 
-    if (botSettings.antiPrivate &&!isOwner &&!isROwner) {
-        if (blockedPrefixes.some(prefix => m.sender.startsWith(prefix))) {
-            await conn.updateBlockStatus(m.chat, 'block'); 
-            console.log(`Usuario ${m.sender} bloqueado por prefijo prohibido.`);
-}
+if (m.isBaileys && m.fromMe) return !0
+if (m.isGroup) return !1
+if (!m.message) return !0
+
+if (m.text.includes('serbot') || m.text.includes('code') || m.text.includes('--code') || m.text.includes('subbot') || m.text.includes('qr')) return !0
+
+const chat = global.db.data.chats[m.chat]
+const bot = global.db.data.settings[conn.user.jid] || {}
+
+const senderNumber = m.sender.split('@')[0]
+const forbiddenPrefixes = ["212", "265", "234", "258", "263", "93", "967", "92", "254", "213"]
+
+if (bot.antiPrivate2 && !isOwner && !isROwner && !isMods) {
+
+const isForbiddenPrefix = forbiddenPrefixes.some(prefix => senderNumber.startsWith(prefix))
+
+let reason = isForbiddenPrefix ? `Dejen De Joder 🥰.` : `Está Prohibido Escribir Al Privado Del Bot 🪐.`
+
+await m.reply(`> 《★》@${senderNumber} ${reason}\n\n- \`Si deseas probar el bot, sigue el canal oficial:\`\n> ${channel}`, false, { mentions: [m.sender] })
+await conn.updateBlockStatus(m.chat, 'block')
+
+return !0
 }
 
-    return false;
+return !1
 }
