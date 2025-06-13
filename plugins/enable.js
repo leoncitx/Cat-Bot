038e4caec0f0ba66ff2dc2d5e4df15c812d82cdb
Wichi <sebastianbarbaro82@gmail.com>
2025-06-13 16:52:03 -0400

    Actualizar enable.js

--- plugins/enable.js
+++ plugins/enable.js
@@ -23,6 +23,19 @@ let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isR
       chat.bienvenida = isEnable;
       break;
 
+    case 'antiara':
+  if (!m.isGroup) {
+    if (!isOwner) {
+      global.dfail('group', m, conn);
+      throw false;
+}
+} else if (!isAdmin) {
+    global.dfail('admin', m, conn);
+    throw false;
+}
+  chat.antiAra = isEnable;
+  break;
+
     case 'antilag':
       if (!m.isGroup) {
         if (!isOwner) {
