3eaced46da4ed20daa210671ba84433fa8c62ead
Wichi <sebastianbarbaro82@gmail.com>
2025-06-13 13:53:19 -0400

    Actualizar main-menu2.js

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -1,5 +1,5 @@
 let handler = async (m, { isPrems, conn }) => {
-let img = 'https://qu.ax/iVZTn.jpg' 
+let img = 'https://files.catbox.moe/ltq7ph.jpg' 
 let texto = `*☁️ _M E N U - A U D I O S_ ☁️*
 
 ° _Tunometecabrasaramambiche_.
