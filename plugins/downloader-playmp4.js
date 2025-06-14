045ab9179d479ecfa72743066e90fc06e4eb61eb
Wichi <sebastianbarbaro82@gmail.com>
2025-06-14 11:04:25 -0400

    Actualizar downloader-playmp4.js

--- plugins/downloader-playmp4.js
+++ plugins/downloader-playmp4.js
@@ -1,93 +1,54 @@
-import fetch from 'node-fetch'
-import yts from 'yt-search'
 
-let handler = async (m, { conn, command, text, usedPrefix }) => {
-  if (!text) throw m.reply(`✧ Ejemplo: ${usedPrefix}${command} Waguri Edit`);
+import fetch from 'node-fetch';
 
- await conn.sendMessage(m.chat, { react: { text: '🕒', key: m.key }})
+let handler = async (m, { conn, args, text}) => {
+  if (!args[0]) throw m.reply('Proporcione una consulta');
 
-    let results = await yts(text);
-    let tes = results.videos[0]
+  const sender = m.sender.split('@')[0];
 
-  const args = text.split(' ');
-  const videoUrl = args[0];
-  
-  const apiUrl = `https://www.apis-anomaki.zone.id/downloader/ytv?url=${encodeURIComponent(tes.url)}`;
+  // Obtener información de la API
+  let ouh = await fetch(`https://fastrestapis.fasturl.cloud/downup/ytdown-v1?name=${text}&format=mp4&quality=720&server=auto`);
+  let gyh = await ouh.json();
 
-  try {
-    const respuesta = await fetch(apiUrl);
-    const keni = await respuesta.json()
-    const { url, qualityLabel, fps } = keni.result.formats[0];
-    const { title } = keni.result;
+  const { duration, thumbnail, views, description, lengthSeconds, uploadDate} = gyh.result.metadata;
+  const { author, name, bio, image, subCount} = gyh.result.author;
+  const { url, format, quality, media, title} = gyh.result;
 
-    if (!url) throw m.reply('No hay respuesta de la api.');
+  m.reply('Procesando solicitud...');
 
+  let textcap = `*YOUTUBE VIDEO DOWNLOADER*\n\n
+  📌 *Título:* ${title}\n
+  ⏳ *Duración:* ${lengthSeconds} segundos\n
+  🎥 *Calidad:* ${quality}\n\n
+  📝 *Descripción:*\n${description}\n\n> ${wm}`;
 
-    const caption = `
-      *💮 PLAY VIDEO 💮*
- 
-  ✧ : \`titulo;\` ${tes.title || 'no encontrado'}
-  ✧ : \`duracion;\` ${tes.duration || 'no encontrado'}
-  ✧ : \`calidad;\` ${qualityLabel || 'no encontrado'}
-  ✧ : \`fps;\` ${fps || 'no encontrado'}
- 
-> ${wm}
-> Pedido de @${m.sender.split('@')[0]}`;
+  // Enviar imagen y detalles primero
+  await conn.sendMessage(
+    m.chat,
+    {
+      image: { url: thumbnail},
+      caption: textcap,
+      mentions: [m.sender],
+},
+    { quoted: m}
+);
 
-//await conn.sendMessage(m.chat, { document: { url: url }, caption: caption, mimetype: 'video/mp4', fileName: `${title}` + `.mp4`}, {quoted: m })
-
-    await conn.sendMessage(m.chat, {
-      video: { url: url },
-      mimetype: "video/mp4",
-      fileName: title,
-      caption,
-      mentions: [m.sender]
-    }, { quoted: m });
-await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key }})
-
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    await conn.sendMessage(m.chat, { react: { text: '❎', key: m.key }})
-  }
+  // Enviar el video como documento MP4 con el nombre de la música
+  await conn.sendMessage(
+    m.chat,
+    {
+      document: { url: media},
+      mimetype: 'video/mp4',
+      fileName: `${title}.mp4`,
+      caption: `📁 *Aquí está tu video en documento*`,
+      mentions: [m.sender],
+},
+    { quoted: m}
+);
 };
 
-handler.help = ['playvideo *<consulta>*'];
+handler.help = ['play2 <consulta>'];
 handler.tags = ['downloader'];
-handler.command = /^(playvideo|playvid)$/i;
+handler.command = ["play2"];
 
-handler.register = true
-handler.disable = false
-
-export default handler
-
-
-/*{
-  "status": true,
-  "creator": "Xyzan - Anomaki",
-  "result": {
-    "status": "success",
-    "formats": [
-      {
-        "itag": 18,
-        "mimeType": "video/mp4; codecs=\"avc1.42001E, mp4a.40.2\"",
-        "bitrate": 679679,
-        "width": 640,
-        "height": 360,
-        "lastModified": "1748374401371375",
-        "contentLength": "12514605",
-        "quality": "medium",
-        "fps": 30,
-        "qualityLabel": "360p",
-        "projectionType": "RECTANGULAR",
-        "averageBitrate": 679435,
-        "audioQuality": "AUDIO_QUALITY_LOW",
-        "approxDurationMs": "147353",
-        "audioSampleRate": "44100",
-        "audioChannels": 2,
-        "qualityOrdinal": "QUALITY_ORDINAL_360P",
-        "url": "https://redirector.googlevideo.com/videoplayback?expire=1749917496&ei=2EpNaIvOJNugzPsP8JmRwQc&ip=176.1.132.81&id=o-AGO9qj_josGpkFezjbicwMQ3NQG1QRSx_YEFCajdKhkJ&itag=18&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&met=1749895896%2C&mh=zy&mm=31%2C29&mn=sn-uxax4vopj5qx-q0n6%2Csn-4g5ednd7&ms=au%2Crdu&mv=m&mvi=1&pcm2cms=yes&pl=17&rms=au%2Cau&initcwndbps=1780000&bui=AY1jyLPsq8WSgxzpa4Hek_qJ6nth1dYtgbzZC1UTvxKh9-7jlb7g6XIj3SY2FvIS_lvsI6rzP7dsxRU_&spc=l3OVKdljChCDPNF31leOKVjNZVXMG4stB2qrEZnC4flyLd6Y-kVt1ac&vprv=1&svpuc=1&mime=video%2Fmp4&ns=0RVDH9jDlHmV6yU98Sj70w0Q&rqh=1&gir=yes&clen=12514605&ratebypass=yes&dur=147.353&lmt=1748374401371375&mt=1749895496&fvip=3&fexp=51331020%2C51466697&c=MWEB&sefc=1&txp=4538534&n=CeDJHrJ4RoRYMg&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cbui%2Cspc%2Cvprv%2Csvpuc%2Cmime%2Cns%2Crqh%2Cgir%2Cclen%2Cratebypass%2Cdur%2Clmt&lsparams=met%2Cmh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpcm2cms%2Cpl%2Crms%2Cinitcwndbps&lsig=APaTxxMwRAIgf5fcs6XzrmcFvlbn0ga69ebXLi-sKEyxEkW-9irntxkCIDwJkISDFk-ZuJNoeYrquYzBTuBKx1R8UCGW9sv7kTXl&sig=AJfQdSswRAIgZWxJAyMKKZyDUvEz-zscdj5eatsYfis3gN6jL6Da2CECIC2c1SuXujvpfBJ4cxownfVudtrNe8ggOV_Ev9R3uPUg&pot=MnRSeo2OUa_KU1JG9ZB4_xttIYscGQMCPg6SXX3Msekhg3rXGM4Cvs2Gw4g9NKBBZca6YPTwbudHtmv0I6mJK53vtCBBNcWccHH5SbFwZT1t-AIXg2x0EWRNV7nkmUG8gxjNxUzAWNpyKtwGrTNMqXiztKpSGQ=="
-      }
-    ],
-    "title": "Lamine Yamal - Y QUE FUE? (Don Miguelo)"
-  }
-}*/
+export default handler;
