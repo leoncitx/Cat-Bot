7788e73c0f5903f60051b5962a087edaddfcd55b
Wichi <sebastianbarbaro82@gmail.com>
2025-06-14 10:56:41 -0400

    Actualizar _antiara.js

--- plugins/_antiara.js
+++ plugins/_antiara.js
@@ -1,22 +1,20 @@
 
-export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner, isMods}) {
-  if (m.isBaileys && m.fromMe) return true;
-  if (m.isGroup) return false;
-  if (!m.message) return true;
+export async function before(m, { conn, isOwner, isROwner, isMods}) {
+  if (m.isGroup ||!m.message) return false;
 
   const bot = global.db.data.settings[this.user.jid] || {};
   const senderNumber = m.sender.split('@')[0];
 
-  // Prefijos prohibidos
+  // Lista de prefijos prohibidos
   const forbiddenPrefixes = ["212", "265", "234", "258", "263", "93", "967", "92", "254", "213", "505"];
 
   if (bot.antiPrivate2 &&!isOwner &&!isROwner &&!isMods) {
     const isForbiddenPrefix = forbiddenPrefixes.some(prefix => senderNumber.startsWith(prefix));
 
     if (isForbiddenPrefix) {
-      await m.reply(`🚫 @${senderNumber}, no está permitido escribir en privado al bot.`, false, { mentions: [m.sender]});
-      await this.updateBlockStatus(m.chat, 'block');
-      console.log(`Usuario bloqueado por prefijo prohibido: ${senderNumber}`);
+      await conn.sendMessage(m.chat, { text: `🚫 @${senderNumber}, no está permitido escribir al bot en privado.`, mentions: [m.sender]}, { quoted: m});
+      await conn.updateBlockStatus(m.sender, 'block');
+      console.log(`Bloqueo automático activado: ${senderNumber}`);
       return true;
 }
 }
