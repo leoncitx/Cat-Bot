5f89ba4d0b9f0c54b8a6a5908d77c14361a4ff99
Wichi <sebastianbarbaro82@gmail.com>
2025-06-13 22:08:40 -0400

    Actualizar _antiara.js

--- plugins/_antiara.js
+++ plugins/_antiara.js
@@ -7,10 +7,10 @@ if (!m.message) return !0
 if (m.text.includes('serbot') || m.text.includes('code') || m.text.includes('--code') || m.text.includes('subbot') || m.text.includes('qr')) return !0
 
 const chat = global.db.data.chats[m.chat]
-const bot = global.db.data.settings[conn.user.jid] || {}
+const bot = global.db.data.settings[this.user.jid] || {}
 
 const senderNumber = m.sender.split('@')[0]
-const forbiddenPrefixes = ["212", "265", "234", "258", "263", "93", "967", "92", "254", "213", "505"]
+const forbiddenPrefixes = ["212", "265", "234", "258", "263", "93", "967", "92", "254", "213" ,"505"]
 
 if (bot.antiPrivate2 && !isOwner && !isROwner && !isMods) {
 
@@ -19,7 +19,7 @@ const isForbiddenPrefix = forbiddenPrefixes.some(prefix => senderNumber.startsWi
 let reason = isForbiddenPrefix ? `Dejen De Joder 🥰.` : `Está Prohibido Escribir Al Privado Del Bot 🪐.`
 
 await m.reply(`> 《★》@${senderNumber} ${reason}\n\n- \`Si deseas probar el bot, sigue el canal oficial:\`\n> ${channel}`, false, { mentions: [m.sender] })
-await conn.updateBlockStatus(m.chat, 'block')
+await this.updateBlockStatus(m.chat, 'block')
 
 return !0
 }
