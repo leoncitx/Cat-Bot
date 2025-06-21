56d64a411a2f4a5ee3e119131bd2a261e6a1e496
Wichi <sebastianbarbaro82@gmail.com>
2025-06-21 12:04:31 -0400

    Actualizar main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,24 +1,27 @@
-
-import PhoneNumber from 'awesome-phonenumber';
-
-async function handler(m, { conn }) { 
+async function handler(m, { conn }) {
     let numcreador = '584146277368';
     let ownerJid = numcreador + '@s.whatsapp.net';
 
-    let name = await conn.getName(ownerJid) || 'Owner'; 
+    let name = await conn.getName(ownerJid) || 'Owner';
     let about = (await conn.fetchStatus(ownerJid).catch(() => {}))?.status || 'Creador de bots de WhatsApp y del Bot Barboza Ai';
     let empresa = 'Barboza- Servicios Tecnológicos';
-    let imagen = 'https://files.catbox.moe/ltq7ph.jpg'; // Reemplaza con la URL de la imagen que deseas mostrar
 
-    // Enviar imagen junto con el número del dueño y sus detalles
-    await conn.sendMessage(m.chat, { 
-        image: { url: imagen },
-        caption: `👤 *Dueño del bot*\n📌 *Nombre:* ${name}\n📞 *Número:* wa.me/${numcreador}\n📝 *Descripción:* ${about}\n🏢 *Empresa:* ${empresa}\n📧 *Email:* sebastianbarbaro82@gmail.com\n🌐 *Instagram:* https://www.instagram.com/sebastian_barboza13`,
-    }, { quoted: m });
+    let caption = `
+✨ *Información del Dueño:* ✨
+
+*👤 Nombre:* ${name}
+*📞 Número:* wa.me/${numcreador}
+*📝 Descripción:* ${about}
+*🏢 Empresa:* ${empresa}
+*📧 Email:* sebastianbarbaro82@gmail.com
+*📸 Instagram:* https://www.instagram.com/sebastian_barboza13
+`;
+
+    await conn.sendMessage(m.chat, { text: caption }, { quoted: m });
 }
 
-handler.help = ['owner']; 
-handler.tags = ['main']; 
+handler.help = ['owner'];
+handler.tags = ['main'];
 handler.command = ['owner', 'creator', 'creador', 'dueño'];
 
 export default handler;
