8c18afb6b531c12812606a3d088e6c7b7d0d2207
Wichi <sebastianbarbaro82@gmail.com>
2025-06-21 14:01:21 -0400

    Actualizar group-banchat.js

--- plugins/group-banchat.js
+++ plugins/group-banchat.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn, isAdmin, isROwner }) => {
     if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = true
-    await conn.reply(m.chat, `🧑‍💻BARBOZABOT FUE DESACTIVADO EN ESTE CHAT`, m, rcanal)
-    await m.react('✅')
+    await conn.reply(m.chat, `🚫 BARBOZABOT FUE DESACTIVADO EN ESTE CHAT`, m, rcanal)
+    await m.react('☑️')
 }
 handler.help = ['banearbot']
 handler.tags = ['group']
