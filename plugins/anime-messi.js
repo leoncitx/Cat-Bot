5305b98658c6bd9e936d0be4e3fbf92d7ba23325
Wichi <sebastianbarbaro82@gmail.com>
2025-06-21 13:42:18 -0400

    Actualizar anime-messi.js

--- plugins/anime-messi.js
+++ plugins/anime-messi.js
@@ -1,27 +1,19 @@
+
 import axios from "axios";
 
-let handler = async (m, { conn, usedPrefix, command }) => {
-    let res = (await axios.get(`https://raw.githubusercontent.com/davidprospero123/api-anime/main/BOT-JSON/Messi.json`)).data;
-    let url = res[Math.floor(Math.random() * res.length)];
+let handler = async (m, { conn}) => {
+  let res = (await axios.get(`https://raw.githubusercontent.com/davidprospero123/api-anime/main/BOT-JSON/Messi.json`)).data;
+  let url = res[Math.floor(Math.random() * res.length)];
 
-    const buttons = [
-        {
-            buttonId: `${usedPrefix + command}`,
-            buttonText: { displayText: "⚽ Ver más" },
-            type: 1
-        }
-    ];
-
-    await conn.sendMessage(
-        m.chat,
-        {
-            image: { url },
-            caption: "*Messi*",
-            buttons: buttons,
-            viewOnce: true
-        },
-        { quoted: m }
-    );
+  await conn.sendMessage(
+    m.chat,
+    {
+      image: { url},
+      caption: "*Messi*",
+      viewOnce: true
+},
+    { quoted: m}
+);
 };
 
 handler.help = ['messi'];
