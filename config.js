b80682fd2aab7660abadb9816976b6585b928c4c
Wichi <sebastianbarbaro82@gmail.com>
2025-06-24 07:35:56 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -55,9 +55,8 @@ global.catalogo = fs.readFileSync('./storage/img/catalogo.png');
 global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
 
 global.ch = {
-ch1: '120363375378707428@newsletter',
-ch2: '120363414007802886@newsletter',
-ch3: '120363419364337473@newsletter'
+ch1: '120363414007802886@newsletter',
+ch2: '120363419364337473@newsletter'
 }
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
