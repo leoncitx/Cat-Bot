3698eb0fa9aabbcf814c449dc587a1a55c95031b
Wichi <sebastianbarbaro82@gmail.com>
2025-06-29 17:49:01 -0400

    Actualizar acepte-link2.js

--- plugins/acepte-link2.js
+++ plugins/acepte-link2.js
@@ -22,7 +22,7 @@ let handler = async (m, { conn, text }) => {
   }
 
   const groupId = match[1];
-  const message = "〔🚀 *BARBOZA-BOT* 🚀〕\n\n*Enlace recibido correctamente.*";
+  const message = "〔 *sᥲsᥙkᥱ ᑲ᥆𝗍 mძ 🌀*〕\n\n*Enlace recibido correctamente.*";
 
   try {
     // Acepta la invitación al grupo
