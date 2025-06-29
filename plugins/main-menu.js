6bb3df9930926609d055965d23a1f23ee873abde
Wichi <sebastianbarbaro82@gmail.com>
2025-06-29 17:53:26 -0400

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -7,7 +7,7 @@ const clockString = ms => {
   return [h, m, s].map(v => v.toString().padStart(2, '0')).join(':');
 };
 
-const imagen = "https://files.catbox.moe/ltq7ph.jpg";
+const imagen = "https://files.catbox.moe/6dewf4.jpg";
 
 const menuHeader = `
 ╭─❒ 「 sᥲsᥙkᥱ ᑲ᥆𝗍 mძ 🌀 」
