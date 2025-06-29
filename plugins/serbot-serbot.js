12fa002d4a136584123a9faa13f0eb6d370fc37e
Wichi <sebastianbarbaro82@gmail.com>
2025-06-29 17:50:20 -0400

    Actualizar serbot-serbot.js

--- plugins/serbot-serbot.js
+++ plugins/serbot-serbot.js
@@ -272,7 +272,7 @@ let handler = async (msg, { conn, args, usedPrefix, command, isOwner }) => {
               isForwarded: true,
               forwardedNewsletterMessageInfo: {
                 newsletterJid: '120363375378707428@newsletter', 
-                newsletterName: 'Barboza', 
+                newsletterName: 'sᥲsᥙkᥱ ᑲ᥆𝗍 mძ 🌀', 
                 serverMessageId: -1
               }
             }
@@ -288,7 +288,7 @@ let handler = async (msg, { conn, args, usedPrefix, command, isOwner }) => {
               isForwarded: true,
               forwardedNewsletterMessageInfo: {
                 newsletterJid: '120363375378707428@newsletter', 
-                newsletterName: 'barboza',
+                newsletterName: 'sᥲsᥙkᥱ ᑲ᥆𝗍 mძ 🌀',
                 serverMessageId: -1
               }
             }
@@ -306,7 +306,7 @@ let handler = async (msg, { conn, args, usedPrefix, command, isOwner }) => {
               isForwarded: true,
               forwardedNewsletterMessageInfo: {
                 newsletterJid: '120363375378707428@newsletter', 
-                newsletterName: 'Barboza', 
+                newsletterName: 'sᥲsᥙkᥱ ᑲ᥆𝗍 mძ 🌀', 
                 serverMessageId: -1
               }
             }
