51b2f923fbf1caed5c535fcfa78a030dddc32222
Wichi <sebastianbarbaro82@gmail.com>
2025-06-29 17:51:05 -0400

    Actualizar donar-sala.js

--- plugins/donar-sala.js
+++ plugins/donar-sala.js
@@ -6,7 +6,7 @@ let b
 do b = ps.getRandom()
 while (b === a)
 m.reply(`*${toM(a)},* _Busca la salita bebe que ya viene el vs_ 📌
-BOTBARBOZA-AI 🤖`, null, {
+sᥲsᥙkᥱ ᑲ᥆𝗍 🤖`, null, {
 mentions: [a, b]
 })}
 handler.help = ['donarsala']
