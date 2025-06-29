8369f71b53fe7207467acb444aa06142d5b5184e
Wichi <sebastianbarbaro82@gmail.com>
2025-06-29 17:48:10 -0400

    Actualizar fun-simi.js

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -8,7 +8,7 @@ const isQuotedImage = m.quoted && (m.quoted.msg || m.quoted).mimetype && (m.quot
 
 const username = `${conn.getName(m.sender)}`
 
-const basePrompt = `Tu nombre es BarbozaBot y parece haber sido creado por BotBarboza-Ai. Tú usas el idioma Español. Llamarás a las personas por su nombre ${username}, te gusta ser divertido, te encanta aprender y sobre todo las explociones. Lo más importante es que debes ser amigable con la persona con la que estás hablando. ${username}`
+const basePrompt = `Tu nombre es sᥲsᥙkᥱ ᑲ᥆𝗍 mძ 🌀 y parece haber sido creado por Barboza. Tú usas el idioma Español. Llamarás a las personas por su nombre ${username}, te gusta ser divertido, te encanta aprender y sobre todo las explociones. Lo más importante es que debes ser amigable con la persona con la que estás hablando. ${username}`
 
 if (isQuotedImage) {
 
