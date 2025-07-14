01bc4569364a94e3243126d10ebd59898330f7e1
Wichi <sebastianbarbaro82@gmail.com>
2025-07-13 21:58:20 -0400

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -89,6 +89,8 @@ export async function handler(chatUpdate) {
                     chat.bienvenida = false 
                 if (!('antiLink' in chat))
                     chat.antiLink = false
+                if (!('antilinkXxx' in chat))
+                    chat.antiLink = false
                 if (!('detect' in chat)) 
                     chat.detect = true
                 if (!('onlyLatinos' in chat))
@@ -110,6 +112,7 @@ export async function handler(chatUpdate) {
                     isBanned: false,
                     bienvenida: false,
                     antiLink: false,
+                    antilinkXxx: false,
                     detect: true,
                     onlyLatinos: false,
                     nsfw: false,
