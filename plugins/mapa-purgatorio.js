04418c66392e8a15c0607f1da841d7a44df32c50
Wichi <sebastianbarbaro82@gmail.com>
2025-07-16 20:52:14 -0400

    Actualizar mapa-purgatorio.js

--- plugins/mapa-purgatorio.js
+++ plugins/mapa-purgatorio.js
@@ -26,6 +26,7 @@ global.db.data.users[m.sender].lastcofre = new Date * 1
 handler.help = ['purgatorio']
 handler.tags = ['freefire']
 handler.command = ['purga', 'purgatorio', 'purg'] 
-handler.register = false
+
+
 handler.admin = true
 export default handler
