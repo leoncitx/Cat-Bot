7b6336d480f7e78d374a09be072c81396189b950
Wichi <sebastianbarbaro82@gmail.com>
2025-07-19 00:16:52 -0400

    Actualizar grupo-kick.js

--- plugins/grupo-kick.js
+++ plugins/grupo-kick.js
@@ -1,19 +1,38 @@
-let handler = async (m, { conn, usedPrefix, command }) => {
+var handler = async (m, { conn, participants, usedPrefix, command }) => {
+    if (!m.mentionedJid[0] && !m.quoted) {
+        return conn.reply(m.chat, `${emoji} Debes mencionar a un usuario para poder expulsarlo del grupo.`, m);
+    }
 
-if (!m.mentionedJid[0] && !m.quoted) return m.reply(`✳️ Ingresa el tag de un usuario. Ejemplo :\n\n*${usedPrefix + command}* @tag`) 
-let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
-if (conn.user.jid.includes(user)) return m.reply(`✳️ No puedo hacer un auto kick`)
+    let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
 
-await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply(`✅ Usuario eliminado con éxito`) 
+    const groupInfo = await conn.groupMetadata(m.chat);
+    const ownerGroup = groupInfo.owner || m.chat.split`-`[0] + '@s.whatsapp.net';
+    const ownerBot = global.owner[0][0] + '@s.whatsapp.net';
+    //const nn = conn.getName(m.sender);
 
-}
+    if (user === conn.user.jid) {
+        return conn.reply(m.chat, `*No puedo eliminar el bot del grupo.*`, m,rcanal);
+    }
 
-handler.help = ['kick @user']
-handler.tags = ['group']
-handler.command = ['kick', 'expulsar'] 
-handler.admin = true
-handler.group = true
-handler.botAdmin = true
+    if (user === ownerGroup) {
+        return conn.reply(m.chat, `*No puedo eliminar al propietario del grupo.*`, m,rcanal);
+    }
 
-export default handler
+    if (user === ownerBot) {
+        return conn.reply(m.chat, `*No puedo eliminar al propietario del bot.*`, m,rcanal);
+    }
+
+    await conn.groupParticipantsUpdate(m.chat, [user], 'remove');
+
+//conn.reply(`${suitag}@s.whatsapp.net`, `${emoji} Un Admin Acabo De Eliminar Un Usuario En El Grupo:\n> ${groupMetadata.subject}.`, m, rcanal, );
+};
+
+handler.help = ['kick'];
+handler.tags = ['grupo'];
+handler.command = ['kick','echar','hechar','sacar','ban'];
+handler.admin = true;
+handler.group = true;
+handler.register = true
+handler.botAdmin = true;
+
+export default handler;
