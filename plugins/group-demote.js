86406377e08f935592f415a5f80ab739df0634d6
Wichi <sebastianbarbaro82@gmail.com>
2025-07-18 23:43:04 -0400

    Actualizar group-demote.js

--- plugins/group-demote.js
+++ plugins/group-demote.js
@@ -1,35 +1,41 @@
-let handler = async (m, { conn,usedPrefix, command, text }) => {
-if(isNaN(text) && !text.match(/@/g)){
+let handler = async (m, { conn, usedPrefix, command, text }) => {
+    let user = null;
 
-}else if(isNaN(text)) {
-var number = text.split`@`[1]
-}else if(!isNaN(text)) {
-var number = text
-}
-if(!text && !m.quoted) return conn.reply(m.chat, `🌀 Responda a un mensaje de dicho usuario a cual le quitarás admin.m)
-if(number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `🌀 El número es inválido.`, m)
+    if (m.quoted) {
+        user = m.quoted.sender;
+    } else if (text) {
+     
+        let numberMatch = text.match(/\d+/);
+        if (numberMatch && numberMatch[0].length >= 11 && numberMatch[0].length <= 13) {
+            user = numberMatch[0] + '@s.whatsapp.net';
+        } else {
+            return conn.reply(m.chat, `🌀 Por favor, responda al mensaje de un aquel usuario que le quitarás admin .`, m,rcanal);
+        }
+    } else if (m.mentionedJid && m.mentionedJid[0]) {
+        user = m.mentionedJid[0];
+    } else {
+        return conn.reply(m.chat, `🌀 Por favor, responda al mensaje de un usuario.`, m,rcanal);
+    }
 
-try {
-if(text) {
-var user = number + '@s.whatsapp.net'
-} else if(m.quoted.sender) {
-var user = m.quoted.sender
-} else if(m.mentionedJid) {
-var user = number + '@s.whatsapp.net'
-} 
-} catch (e) {
-} finally {
-conn.groupParticipantsUpdate(m.chat, [user], 'demote')
-m.reply(`✅ Usuario degradado con éxito.`)
-}
+    if (!user) {
+        return conn.reply(m.chat, `🌀 No se pudo identificar al usuario.`, m);
+    }
 
-}
-handler.help = ['demote']
-handler.tags = ['group']
-handler.command = ['demote', 'degradar'] 
-handler.group = true
-handler.admin = true
-handler.botAdmin = true
-handler.fail = null
+    try {
+        await conn.groupParticipantsUpdate(m.chat, [user], 'demote');
+        m.reply(`✅ Usuario degradado con éxito.`);
+    } catch (e) {
+        console.error(e);
+        m.reply(`❌ Ha ocurrido un error al intentar degradar al usuario.`);
+    }
+};
 
-export default handler
+handler.help = ['demote'];
+handler.tags = ['group'];
+handler.command = ['demote', 'degradar'];
+handler.group = true;
+handler.admin = true;
+handler.botAdmin = true;
+handler.fail = null;
+
+export default handler;
