af6bb963a03d7c23f2dc2b0fa46828387267878b
Wichi <sebastianbarbaro82@gmail.com>
2025-07-19 00:21:03 -0400

    Actualizar grupo-kick.js

--- plugins/grupo-kick.js
+++ plugins/grupo-kick.js
@@ -1,34 +1,17 @@
-import axios from 'axios';
-
 let handler = async (m, { conn, usedPrefix, command }) => {
 
-  if (!m.mentionedJid[0] && !m.quoted) return m.reply(`✳️ Ingresa el tag de un usuario. Ejemplo :\n\n*${usedPrefix + command}* @tag`)
+if (!m.mentionedJid[0] && !m.quoted) return m.reply(`✳️ Ingresa el tag de un usuario. Ejemplo :\n\n*${usedPrefix + command}* @tag`) 
+let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
+if (conn.user.jid.includes(user)) return m.reply(`✳️ No puedo hacer un auto kick`)
 
-  let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
-  if (conn.user.jid.includes(user)) return m.reply(`✳️ No puedo hacer un auto kick`)
-
-  await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-
-  // Descarga la miniatura desde una URL
-  let thumbnail = await axios.get('https://qu.ax/VGCPX.jpg', { responseType: 'arraybuffer' })
-
-  await conn.sendMessage(m.chat, {
-    text: `✅ Usuario eliminado con éxito`,
-    contextInfo: {
-      externalAdReply: {
-        mediaType: 1,
-        renderLargerThumbnail: true,
-        thumbnail: Buffer.from(thumbnail.data), // Aquí va la miniatura en formato binario
-        sourceUrl: 'https://github.com/Elrebelde1'
-      }
-    }
-  }, { quoted: m })
+await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
+m.reply(`✅ Usuario eliminado con éxito`) 
 
 }
 
 handler.help = ['kick @user']
 handler.tags = ['group']
-handler.command = ['kick', 'expulsar']
+handler.command = ['kick', 'expulsar'] 
 handler.admin = true
 handler.group = true
 handler.botAdmin = true
