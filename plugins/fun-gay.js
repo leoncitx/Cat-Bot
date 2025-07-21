db8f44be96bae9f648286cb18212f5e41e94b340
Wichi <sebastianbarbaro82@gmail.com>
2025-07-21 13:38:54 -0400

    Actualizar fun-gay.js

--- plugins/fun-gay.js
+++ plugins/fun-gay.js
@@ -1,13 +1,29 @@
-let handler = async (m, { groupMetadata }) => {
-   let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
-   let nro = (100).getRandom()
-   await m.reply(`@${who.split("@")[0]} es ${nro}% Gay 🏳️‍🌈.`, null, { mentions: [who] })
-}
 
-handler.help = ['gay']
-handler.tags = ['fun']
-handler.command = ['cekgay', 'gay2']
 
-handler.group = true
+let handler = async (m, { conn, groupMetadata}) => {
+  let who = m.mentionedJid[0]
+? m.mentionedJid[0]
+: m.quoted
+? m.quoted.sender
+: m.sender;
 
-export default handler
+  let nro = Math.floor(Math.random() * 101); // Valor entre 0 y 100
+  let mensaje = `@${who.split("@")[0]} es ${nro}% Gay 🏳️‍🌈.`;
+
+  await m.reply(mensaje, null, { mentions: [who]});
+
+  await conn.sendFile(
+    m.chat,
+    'https://qu.ax/lQSxP.mp3',
+    'audio.mp3',
+    null,
+    m,
+);
+};
+
+handler.help = ['gay'];
+handler.tags = ['fun'];
+handler.command = ['cekgay', 'gay2'];
+handler.group = true;
+
+export default handler;
