ddfa034e849921657d000db13833ddc55a37db7b
Wichi <sebastianbarbaro82@gmail.com>
2025-07-22 12:41:10 -0400

    Update _bot-audios.js

--- plugins/_bot-audios.js
+++ plugins/_bot-audios.js
@@ -408,7 +408,7 @@ let handler = async (m, { conn }) => {
 
          case 'zzzz':
             conn.sendPresenceUpdate('recording', m.chat);
-            await conn.sendFile(m.chat, 'https://cdn.russellxz.click/ec338afa.mp3', `${text}.mp3`, null, m, true, { type: 'audioMessage' });
+            await conn.sendFile(m.chat, 'https://cdn.russellxz.click/dff04354.mp3', `${text}.mp3`, null, m, true, { type: 'audioMessage' });
             break;
 
         case 'las reglas del grupo':
