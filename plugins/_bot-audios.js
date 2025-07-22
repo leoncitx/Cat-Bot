ff666af4c370453ab7bea0b39dcb4969bf4b5518
Wichi <sebastianbarbaro82@gmail.com>
2025-07-22 14:03:37 -0400

    Actualizar _bot-audios.js

--- plugins/_bot-audios.js
+++ plugins/_bot-audios.js
@@ -623,7 +623,7 @@ let handler = async (m, { conn }) => {
 
         case 'gemidos':
             conn.sendPresenceUpdate('recording', m.chat);
-            await conn.sendFile(m.chat, 'https://cdn.russellxz.click/c942ca03.mp3', `${text}.mp3`, null, m, true, { type: 'audioMessage' });
+            await conn.sendFile(m.chat, 'https://cdn.russellxz.click/907939e4.mp3', `${text}.mp3`, null, m, true, { type: 'audioMessage' });
             break;
 
         case 'audio hentai':
