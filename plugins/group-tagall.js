8a560ffa2f39bb6b747893d41d7685456994ac7b
Wichi <sebastianbarbaro82@gmail.com>
2025-07-24 16:27:25 -0400

    Actualizar group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -78,7 +78,7 @@ const handler = async (m, { isOwner, isAdmin, conn, text, participants, args}) =
 handler.help = ['todos'];
 handler.tags = ['group'];
 handler.command = /^(tagall|invocar|marcar|todos|invocación)$/i;
-handler.admin = true;
+handler.admin = false;
 handler.group = true;
 
 export default handler;
