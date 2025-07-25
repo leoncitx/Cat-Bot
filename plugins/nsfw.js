b37afa588b8f8c20025077bc004239268697443b
Wichi <sebastianbarbaro82@gmail.com>
2025-07-25 18:38:28 -0400

    Actualizar nsfw.js

--- plugins/nsfw.js
+++ plugins/nsfw.js
@@ -1,3 +1,62 @@
+import fetch from 'node-fetch';
+
+const handler = async (m, { conn, command, usedPrefix}) => {
+  if (!db.data.chats[m.chat].nsfw && m.isGroup) throw '🚩 *¡Estos comandos están desactivados!*';
+
+  switch (command) {
+    case 'pack':
+      const url = await pack[Math.floor(Math.random() * pack.length)];
+      conn.sendMessage(m.chat, {
+        image: { url},
+        caption: `*🥵 Pack 🥵*`,
+        footer: dev
+}, { quoted: m});
+      break;
+
+    case 'pack2':
+      const url2 = await packgirl[Math.floor(Math.random() * packgirl.length)];
+      conn.sendMessage(m.chat, {
+        image: { url: url2},
+        caption: `*🥵 Pack 2 🥵*`,
+        footer: dev
+}, { quoted: m});
+      break;
+
+    case 'pack3':
+      const url3 = await packmen[Math.floor(Math.random() * packmen.length)];
+      conn.sendMessage(m.chat, {
+        image: { url: url3},
+        caption: `*🥵 Pack 3 🥵*`,
+        footer: dev
+}, { quoted: m});
+      break;
+
+    case 'videoxxx':
+    case 'vídeoxxx':
+      const url4 = await videosxxxc[Math.floor(Math.random() * videosxxxc.length)];
+      conn.sendMessage(m.chat, {
+        video: { url: url4},
+        caption: `*ᴅɪsғʀᴜᴛᴀ ᴅᴇʟ ᴠɪᴅᴇᴏ 🥵*`,
+        footer: dev
+}, { quoted: m});
+      break;
+
+    case 'videoxxxlesbi':
+    case 'videolesbixxx':
+    case 'pornolesbivid':
+    case 'pornolesbianavid':
+    case 'pornolesbiv':
+    case 'pornolesbianav':
+    case 'pornolesv':
+      const url5 = await videosxxxc2[Math.floor(Math.random() * videosxxxc2.length)];
+      conn.sendMessage(m.chat, {
+        video: { url: url5},
+        caption: `*ᴅɪsғʀᴜᴛᴀ ᴅᴇʟ ᴠɪᴅᴇᴏ 🥵*`,
+        footer: dev
+}, { quoted: m});
+      break;
+}
+};
 
 handler.help = ['pack', 'pack2', 'pack3', 'videoxxx', 'vídeoxxx', 'videoxxxlesbi', 'videolesbixxx', 'pornolesbivid', 'pornolesbianavid', 'pornolesbiv', 'pornolesbianav', 'pornolesv'];
 handler.command = ['pack', 'pack2', 'pack3', 'videoxxx', 'vídeoxxx', 'videoxxxlesbi', 'videolesbixxx', 'pornolesbivid', 'pornolesbianavid', 'pornolesbiv', 'pornolesbianav', 'pornolesv'];
