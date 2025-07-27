d77ab8332685e8fbe47b3aa393613b8beff4275a
Wichi <sebastianbarbaro82@gmail.com>
2025-07-27 18:47:30 -0400

    Actualizar tools-balogo.js

--- plugins/tools-balogo.js
+++ plugins/tools-balogo.js
@@ -2,28 +2,49 @@ import jimp from "jimp"
 import { S_WHATSAPP_NET } from '@whiskeysockets/baileys'
  
 let handler = async (m, { conn, usedPrefix, command, args, isOwner }) => {
-try {
-let groupId = m.chat;
-let quotedMsg = m.quoted ? m.quoted : m
-if (!m.quoted) return m.reply(`*⚠️ Responde a una Imagen.*`)
-let mediaType = (quotedMsg.type || quotedMsg).mimetype || '';
-var media = await quotedMsg.download();
-async function processImage(media) {
-const image = await jimp.read(media);
-const resizedImage = image.getWidth() > image.getHeight() ? image.resize(720, jimp.AUTO) : image.resize(jimp.AUTO, 720);
-return { img: await resizedImage.getBufferAsync(jimp.MIME_JPEG),
-}}
-var { img: processedImage } = await processImage(media);
-conn.query({ tag: 'iq', attrs: { target: groupId, to: S_WHATSAPP_NET, type:'set', xmlns: 'w:profile:picture' },
-content: [{ tag: 'picture', attrs: { type: 'image' }, content: processedImage }]})
-m.react("✅️")
-} catch (error) {
-console.log(error)
-return m.react('❌');
-} 
-};
-handler.help = ['setppgc'];
-handler.tags = ['group'];
-handler.command = /^setpp(group|grup|gc)?$/i
+  try {
+    let quotedMsg = m.quoted ? m.quoted : m
+    if (!m.quoted) return m.reply(`*⚠️ Responde a una Imagen.*`)
 
-export default handler
+    let mediaType = (quotedMsg.type || quotedMsg).mimetype || '';
+    var media = await quotedMsg.download();
+    
+    async function processImage(media) {
+      const image = await jimp.read(media);
+      const resizedImage = image.getWidth() > image.getHeight()
+        ? image.resize(720, jimp.AUTO)
+        : image.resize(jimp.AUTO, 720);
+      return { img: await resizedImage.getBufferAsync(jimp.MIME_JPEG) }
+    }
+
+    var { img: processedImage } = await processImage(media);
+
+    await conn.query({
+      tag: 'iq',
+      attrs: {
+        to: m.sender,
+        type: 'set',
+        xmlns: 'w:profile:picture'
+      },
+      content: [{
+        tag: 'picture',
+        attrs: { type: 'image' },
+        content: processedImage
+      }]
+    });
+
+    m.react("✅️");
+    await m.reply('✅ Foto de perfil del bot actualizada.');
+    
+  } catch (error) {
+    console.log(error);
+    m.react('❌');
+  } 
+};
+
+handler.help = ['setppbot'];
+handler.tags = ['owner'];
+handler.command = /^setppbot$/i;
+handler.owner = false;
+
+export default handler;
