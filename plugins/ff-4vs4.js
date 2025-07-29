d780340e56c181a8c061854e502d7826f61fb961
Wichi <sebastianbarbaro82@gmail.com>
2025-07-28 23:52:48 -0400

    Actualizar ff-4vs4.js

--- plugins/ff-4vs4.js
+++ plugins/ff-4vs4.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, args, command, usedPrefix}) => {
   if (!args[0]) throw `
 𝟒 𝐕𝐄𝐑𝐒𝐔𝐒 𝟒
 
-⏱ 𝐇𝐎𝐑𝐀𝐑𝐈𝐎                       •
+⏱ 𝐇𝐎𝐑𝐀𝐑𝐈𝐎
 🇲🇽 𝐌𝐄𝐗𝐈𝐂𝐎:
 🇨🇴 𝐂𝐎𝐋𝐎𝐌𝐁𝐈𝐀:
 
@@ -20,15 +20,44 @@ let handler = async (m, { conn, args, command, usedPrefix}) => {
     🥷🏻 ┇
     🥷🏻 ┇
 
-    ㅤʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄𝐒:
+    ʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄𝐒:
     🥷🏻 ┇
     🥷🏻 ┇
 `
 
+  const fkontak = {
+    key: {
+      participants: "0@s.whatsapp.net",
+      remoteJid: "status@broadcast",
+      fromMe: false,
+      id: "AlienMenu"
+},
+    message: {
+      locationMessage: {
+        name: "INVOCACIÓN MASIVA 👽",
+        jpegThumbnail: await (await fetch('https://files.catbox.moe/1j784p.jpg')).buffer(),
+        vcard:
+          "BEGIN:VCARD\n" +
+          "VERSION:3.0\n" +
+          "N:;Sasuke;;;\n" +
+          "FN:Sasuke Bot\n" +
+          "ORG:Kaneki Developers\n" +
+          "TITLE:\n" +
+          "item1.TEL;waid=19709001746:+1 (970) 900-1746\n" +
+          "item1.X-ABLabel:Alien\n" +
+          "X-WA-BIZ-DESCRIPTION:🛸 Llamado grupal universal con estilo.\n" +
+          "X-WA-BIZ-NAME:Sasuke\n" +
+          "END:VCARD"
+}
+},
+    participant: "0@s.whatsapp.net"
+}
+
   await conn.sendMessage(m.chat, {
     image: { url: 'https://cdn.russellxz.click/16b3faeb.jpeg'},
-    caption: `𝟒 𝐕𝐒 𝟒\n\n⏱ 𝐇𝐎𝐑𝐀𝐑𝐈𝐎\n🇲🇽 𝐌𝐄𝐗𝐈𝐂𝐎: ${args[0]}\n🇨🇴 𝐂𝐎𝐋𝐎𝐌𝐁𝐈𝐀: ${args[0]}\n\n➥ 𝐌𝐎𝐃𝐀𝐋𝐈𝐃𝐀𝐃: \n➥ 𝐉𝐔𝐆𝐀𝐃𝐎𝐑𝐄𝐒:\n\n👑 ┇ \n🥷🏻 ┇  \n🥷🏻 ┇ \n🥷🏻 ┇ \n\nʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄𝐒:\n🥷🏻 ┇ \n🥷🏻 ┇`
-}, { quoted: m})
+    caption: `𝟒 𝐕𝐒 𝟒\n\n⏱ 𝐇𝐎𝐑𝐀𝐑𝐈𝐎\n🇲🇽 𝐌𝐄𝐗𝐈𝐂𝐎: ${args[0]}\n🇨🇴 𝐂𝐎𝐋𝐎𝐌𝐁𝐈𝐀: ${args[0]}\n\n➥ 𝐌𝐎𝐃𝐀𝐋𝐈𝐃𝐀𝐃:\n➥ 𝐉𝐔𝐆𝐀𝐃𝐎𝐑𝐄𝐒:\n\n👑 ┇ \n🥷🏻 ┇\n🥷🏻 ┇\n🥷🏻 ┇\n\nʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄𝐒:\n🥷🏻 ┇\n🥷🏻 ┇`,
+    mentions: []
+}, { quoted: fkontak})
 }
 
 handler.help = ['4vs4']
