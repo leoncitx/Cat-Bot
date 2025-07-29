fe47cd66edbdd7c30e7d81ed3ac246cffd920549
Wichi <sebastianbarbaro82@gmail.com>
2025-07-29 00:17:48 -0400

    Actualizar ff-6vs6.js

--- plugins/ff-6vs6.js
+++ plugins/ff-6vs6.js
@@ -1,9 +1,6 @@
-
-import fg from 'api-dylux'
-import fetch from 'node-fetch'
 import axios from 'axios'
 
-let handler = async (m, { conn, args, command, usedPrefix}) => {
+let handler = async (m, { conn, args, command, usedPrefix }) => {
   if (!args[0]) throw `
 *6 𝐕𝐄𝐑𝐒𝐔𝐒 6*
 
@@ -28,7 +25,6 @@ let handler = async (m, { conn, args, command, usedPrefix}) => {
     🥷🏻 ┇
 `
 
-  // Mensaje dinámico tipo fkontak (izumi)
   const textOptions = [
     "𝙀𝙩𝙞𝙦𝙪𝙚𝙩𝙖 𝙂𝙚𝙣𝙚𝙧𝙖𝙡 𝙓 𝙂𝙚𝙣𝙚𝙨𝙞𝙨",
     "𝙈𝙚𝙣𝙘𝙞𝙤𝙣 𝙂𝙚𝙣𝙚𝙧𝙖𝙡",
@@ -41,28 +37,39 @@ let handler = async (m, { conn, args, command, usedPrefix}) => {
 
   const msjRandom = textOptions[Math.floor(Math.random() * textOptions.length)]
   const img = imgOptions[Math.floor(Math.random() * imgOptions.length)]
-  const thumb = Buffer.from((await axios.get(img, { responseType: 'arraybuffer'})).data)
+
+  let thumbBuffer
+  try {
+    const response = await axios.get(img, { responseType: 'arraybuffer' })
+    thumbBuffer = Buffer.from(response.data)
+  } catch (error) {
+    console.error("Error fetching thumbnail image:", error)
+    thumbBuffer = Buffer.from('')
+  }
 
   const izumi = {
-    key: { participants: "0@s.whatsapp.net", fromMe: false, id: "Halo"},
+    key: {
+      fromMe: false,
+      participant: "0@s.whatsapp.net",
+      remoteJid: "status@broadcast"
+    },
     message: {
-      locationMessage: {
-        name: msjRandom,
-        jpegThumbnail: thumb,
-        vcard:
-          "BEGIN:VCARD\nVERSION:3.0\nN:;Unlimited;;;\nFN:Unlimited\nORG:Unlimited\nTITLE:\n" +
-          "item1.TEL;waid=19709001746:+1 (970) 900-1746\nitem1.X-ABLabel:Unlimited\n" +
-          "X-WA-BIZ-DESCRIPTION:ofc\nX-WA-BIZ-NAME:Unlimited\nEND:VCARD"
-}
-},
-    participant: "0@s.whatsapp.net"
-}
+      orderMessage: {
+        itemCount: 99999,
+        message: msjRandom,
+        footerText: "Unlimited",
+        thumbnail: thumbBuffer,
+        surface: 2,
+        sellerJid: "0@s.whatsapp.net"
+      }
+    }
+  }
 
   await conn.sendMessage(m.chat, {
-    image: { url: 'https://cdn.russellxz.click/16b3faeb.jpeg'},
+    image: { url: 'https://cdn.russellxz.click/16b3faeb.jpeg' },
     caption: `*6 𝐕𝐒 6*\n\n⏱ 𝐇𝐎𝐑𝐀𝐑𝐈𝐎\n🇲🇽 𝐌𝐄𝐗𝐈𝐂𝐎: ${args[0]}\n🇨🇴 𝐂𝐎𝐋𝐎𝐌𝐁𝐈𝐀: ${args[0]}\n\n➥ 𝐌𝐎𝐃𝐀𝐋𝐈𝐃𝐀𝐃:\n➥ 𝐉𝐔𝐆𝐀𝐃𝐎𝐑𝐄𝐒:\n\n👑 ┇ \n🥷🏻 ┇\n🥷🏻 ┇\n🥷🏻 ┇\n🥷🏻 ┇\n🥷🏻 ┇\n\nʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄𝐒:\n🥷🏻 ┇\n🥷🏻 ┇`,
     mentions: []
-}, { quoted: izumi})
+  }, { quoted: izumi })
 }
 
 handler.help = ['6vs6']
