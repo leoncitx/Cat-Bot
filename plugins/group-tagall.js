f82026be48bd25d9cf0c16cdb22f45ffe1f83b53
Wichi <sebastianbarbaro82@gmail.com>
2025-07-29 14:25:02 -0400

    Actualizar group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -1,4 +1,5 @@
 import fetch from "node-fetch";
+import axios from 'axios';
 
 const handler = async (m, { isOwner, isAdmin, conn, text, participants, args}) => {
   const chat = global.db.data.chats[m.chat] || {};
@@ -13,76 +14,57 @@ const handler = async (m, { isOwner, isAdmin, conn, text, participants, args}) =
   const groupMetadata = await conn.groupMetadata(m.chat);
   const groupName = groupMetadata.subject;
 
-  const countryFlags = {
-    '1': '🇺🇸', '44': '🇬🇧', '33': '🇫🇷', '49': '🇩🇪', '39': '🇮🇹', '81': '🇯🇵',
-    '82': '🇰🇷', '86': '🇨🇳', '7': '🇷🇺', '91': '🇮🇳', '61': '🇦🇺', '64': '🇳🇿',
-    '34': '🇪🇸', '55': '🇧🇷', '52': '🇲🇽', '54': '🇦🇷', '57': '🇨🇴', '51': '🇵🇪',
-    '56': '🇨🇱', '58': '🇻🇪', '502': '🇬🇹', '503': '🇸🇻', '504': '🇭🇳', '505': '🇳🇮',
-    '506': '🇨🇷', '507': '🇵🇦', '591': '🇧🇴', '592': '🇬🇾', '593': '🇪🇨', '595': '🇵🇾',
-    '596': '🇲🇶', '597': '🇸🇷', '598': '🇺🇾', '53': '🇨🇺', '20': '🇪🇬', '972': '🇮🇱',
-    '90': '🇹🇷', '63': '🇵🇭', '62': '🇮🇩', '60': '🇲🇾', '65': '🇸🇬', '66': '🇹🇭',
-    '31': '🇳🇱', '32': '🇧🇪', '30': '🇬🇷', '36': '🇭🇺', '46': '🇸🇪', '47': '🇳🇴',
-    '48': '🇵🇱', '421': '🇸🇰', '420': '🇨🇿', '40': '🇷🇴', '43': '🇦🇹', '373': '🇲🇩'
-};
+  const textOptions = [
+    "𝙀𝙩𝙞𝙦𝙪𝙚𝙩𝙖 𝙂𝙚𝙣𝙚𝙧𝙖𝙡 𝙓 𝙂𝙚𝙣𝙚𝙨𝙞𝙨",
+    "𝙈𝙚𝙣𝙘𝙞𝙤𝙣 𝙂𝙚𝙣𝙚𝙧𝙖𝙡",
+    "𝙀𝙩𝙞𝙦𝙪𝙚𝙩𝙖𝙣𝙙𝙤 𝙖 𝙡𝙤𝙨 𝙉𝙋𝘾"
+  ];
+  const imgOptions = [
+    "https://iili.io/FKVDVAN.jpg",
+    "https://iili.io/FKVbUrJ.jpg"
+  ];
 
-  const getCountryFlag = (id) => {
-    const phoneNumber = id.split('@')[0];
-    if (phoneNumber.startsWith('1')) return '🇺🇸';
-    let prefix = phoneNumber.substring(0, 3);
-    if (!countryFlags[prefix]) {
-      prefix = phoneNumber.substring(0, 2);
-}
-    return countryFlags[prefix] || '🏳️‍🌈';
-};
+  const msjRandom = textOptions[Math.floor(Math.random() * textOptions.length)];
+  const img = imgOptions[Math.floor(Math.random() * imgOptions.length)];
 
-  let messageText = `*${groupName}*\n\n*Integrantes: ${participants.length}*\n${customMessage}\n┌──⭓ *Despierten*\n`;
-  for (const mem of participants) {
-    messageText += `${emoji} ${getCountryFlag(mem.id)} @${mem.id.split('@')[0]}\n`;
-}
-  messageText += `└───────⭓\n\n𝘚𝘶𝘱𝘦𝘳 𝘉𝘰𝘵 𝘞𝘩𝘢𝘵𝘴𝘈𝘱𝘱 🚩`;
+  const thumb = Buffer.from(
+    (await axios.get(img, { responseType: 'arraybuffer'})).data
+);
 
-  const imageUrl = 'https://files.catbox.moe/1j784p.jpg';
-  const audioUrl = 'https://cdn.russellxz.click/a8f5df5a.mp3';
-
-  const fkontak = {
-    key: {
-      participants: "0@s.whatsapp.net",
-      remoteJid: "status@broadcast",
-      fromMe: false,
-      id: "AlienMenu"
-},
+  const izumi = {
+    key: { participants: "0@s.whatsapp.net", fromMe: false, id: "Halo"},
     message: {
       locationMessage: {
-        name: "*Sasuke Bot MD 🌀*",
-        jpegThumbnail: await (await fetch('https://files.catbox.moe/1j784p.jpg')).buffer(),
+        name: msjRandom,
+        jpegThumbnail: thumb,
         vcard:
-          "BEGIN:VCARD\n" +
-          "VERSION:3.0\n" +
-          "N:;Sasuke;;;\n" +
-          "FN:Sasuke Bot\n" +
-          "ORG:Barboza Developers\n" +
-          "TITLE:\n" +
-          "item1.TEL;waid=19709001746:+1 (970) 900-1746\n" +
-          "item1.X-ABLabel:Alien\n" +
-          "X-WA-BIZ-DESCRIPTION:🛸 Llamado grupal universal con estilo.\n" +
-          "X-WA-BIZ-NAME:Sasuke\n" +
-          "END:VCARD"
+          "BEGIN:VCARD\nVERSION:3.0\nN:;Unlimited;;;\nFN:Unlimited\nORG:Unlimited\nTITLE:\n" +
+          "item1.TEL;waid=19709001746:+1 (970) 900-1746\nitem1.X-ABLabel:Unlimited\n" +
+          "X-WA-BIZ-DESCRIPTION:ofc\nX-WA-BIZ-NAME:Unlimited\nEND:VCARD"
 }
 },
     participant: "0@s.whatsapp.net"
 };
 
+  let messageText = `*${groupName}*\n\n*Integrantes: ${participants.length}*\n${customMessage}\n┌──⭓ *Despierten*\n`;
+  for (const mem of participants) {
+    messageText += `${emoji} @${mem.id.split('@')[0]}\n`;
+}
+  messageText += `└───────⭓\n\n𝘚𝘶𝘱𝘦𝘳 𝘉𝘰𝘵 𝘞𝘩𝘢𝘵𝘴𝘈𝘱𝘱 🚩`;
+
+  const audioUrl = 'https://cdn.russellxz.click/a8f5df5a.mp3';
+
   await conn.sendMessage(m.chat, {
-    image: { url: imageUrl},
+    image: { url: img},
     caption: messageText,
     mentions: participants.map(a => a.id)
-}, { quoted: fkontak});
+}, { quoted: izumi});
 
   await conn.sendMessage(m.chat, {
     audio: { url: audioUrl},
     mimetype: 'audio/mp4',
     ptt: true
-}, { quoted: fkontak});
+}, { quoted: izumi});
 };
 
 handler.help = ['todos'];
