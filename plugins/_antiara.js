ecf33e54a270b6867114f89510f3ce0396668d5c
Wichi <sebastianbarbaro82@gmail.com>
2025-08-03 11:32:36 -0400

    Actualizar _antiara.js

--- plugins/_antiara.js
+++ plugins/_antiara.js
@@ -1,80 +1,171 @@
-import axios from 'axios'
-import * as cheerio from 'cheerio';
-import mime from 'mime-types'
-
-let handler = async (m, { conn, text}) => {
-  if (!text) return m.reply(`📌 Ejemplo:\n.mediafire https://www.mediafire.com/file/xfk1u8yl4uqbizx/nulis.zip/file`)
-  if (!/mediafire\.com/.test(text)) return m.reply(`❌ El enlace no es válido. Solo se aceptan links de *mediafire.com*`)
-
-  try {
-    const { data} = await axios.get('https://api.nekorinn.my.id/tools/rynn-stuff-v2', {
-      params: {
-        method: 'GET',
-        url: text,
-        accessKey: '3ebcf782818cfa0b7265086f112ae25c0954afec762aa05a2eac66580c7cb353'
+function _0x68be(_0x2f8e07, _0x4169cc) {
+    const _0x5491bd = _0xb206();
+    return _0x68be = function (_0x1b1b2d, _0x2de284) {
+        _0x1b1b2d = _0x1b1b2d - (0x19ef + 0x88e + -0x2202);
+        let _0x4d2633 = _0x5491bd[_0x1b1b2d];
+        return _0x4d2633;
+    }, _0x68be(_0x2f8e07, _0x4169cc);
 }
-})
-
-    const $ = cheerio.load(data.result.response)
-    const raw = $('div.dl-info')
-
-    const filename = $('.dl-btn-label').attr('title') || raw.find('div.intro div.filename').text().trim()
-    const extension = filename.split('.').pop()
-    const mimetype = mime.lookup(extension.toLowerCase()) || 'application/octet-stream'
-
-    const filesize = raw.find('ul.details li:nth-child(1) span').text().trim()
-    const fecha = raw.find('ul.details li:nth-child(2) span').text().trim()
-    const urlCodificada = $('a#downloadButton').attr('data-scrambled-url')
-
-    if (!urlCodificada) return m.reply('❌ No se pudo obtener el enlace de descarga.')
-
-    const urlDescarga = Buffer.from(urlCodificada, 'base64').toString()
-    const pesoMB = parseFloat(filesize) * (filesize.toLowerCase().includes('gb')? 1024: 1)
-
-    const mensaje = `
-📦 *Archivo MediaFire*
-📁 *Nombre:* ${filename}
-📄 *Tipo:* ${mimetype}
-📦 *Tamaño:* ${filesize}
-🗓️ *Subido:* ${fecha}
-`.trim()
-
-    const contextInfo = {
-      externalAdReply: {
-        showAdAttribution: true,
-        mediaUrl: urlDescarga,
-        mediaType: 1,
-        renderLargerThumbnail: true,
-        thumbnailUrl: global.thumb || 'https://telegra.ph/file/7e6802b6c40fcf8fc4aa5.jpg',
-        title: filename,
-        body: `${filesize} | Subido: ${fecha}`,
-        sourceUrl: urlDescarga
+const _0xd5aa92 = _0x68be;
+(function (_0x31d09e, _0x409629) {
+    const _0x4313d3 = _0x68be, _0x14e4af = _0x31d09e();
+    while (!![]) {
+        try {
+            const _0x5d0f70 = parseInt(_0x4313d3(0xa9)) / (-0x361 * -0x5 + 0xa19 + 0x1afd * -0x1) * (parseInt(_0x4313d3(0xaa)) / (0x25 * -0xd + 0x13 * 0xc5 + -0x4 * 0x32f)) + parseInt(_0x4313d3(0x9d)) / (0x1876 + -0x344 + 0xbb * -0x1d) + parseInt(_0x4313d3(0xaf)) / (0x18 * -0x60 + -0x7f * 0x22 + -0x1 * -0x19e2) + -parseInt(_0x4313d3(0x7b)) / (-0x1c * -0x107 + -0x232a + 0x66b * 0x1) + parseInt(_0x4313d3(0x9c)) / (-0x66 * 0x2a + 0xb4 + 0x112 * 0xf) * (parseInt(_0x4313d3(0x81)) / (-0x2674 + 0xf1d * -0x1 + -0x7a8 * -0x7)) + -parseInt(_0x4313d3(0x9f)) / (0x1 * -0x10e0 + 0x602 + 0xae6) * (parseInt(_0x4313d3(0x92)) / (0x3d6 + 0x266c + -0x2a39)) + -parseInt(_0x4313d3(0x95)) / (-0x6b1 + -0xa54 + 0x110f) * (parseInt(_0x4313d3(0x84)) / (0x541 + -0x86 * -0xb + 0x8 * -0x15f));
+            if (_0x5d0f70 === _0x409629)
+                break;
+            else
+                _0x14e4af['push'](_0x14e4af['shift']());
+        } catch (_0x43d517) {
+            _0x14e4af['push'](_0x14e4af['shift']());
+        }
+    }
+}(_0xb206, -0x9e3ab + 0xc92 * -0x49 + -0x9978d * -0x2));
+function _0xb206() {
+    const _0x5d1fc7 = [
+        'MToIG',
+        'bbhco',
+        'GjevD',
+        'eyJHL',
+        '\x0a🗓️\x20*Subido:*\x20',
+        'test',
+        'mediafire',
+        'command',
+        'div.intro\x20div.filename',
+        'pop',
+        'text',
+        'FbKdI',
+        '2934LlQlYI',
+        'YFHAw',
+        '3ebcf782818cfa0b7265086f112ae25c0954afec762aa05a2eac66580c7cb353',
+        '2654860fcySmc',
+        '.dl-btn-label',
+        '\x0a📦\x20*Archivo\x20MediaFire*\x0a📁\x20*Nombre:*\x20',
+        'ul.details\x20li:nth-child(1)\x20span',
+        '\x20|\x20Subido:\x20',
+        '❌\x20Error\x20al\x20procesar\x20el\x20enlace:\x20',
+        '📌\x20Ejemplo:\x0a.mediafire\x20https://www.mediafire.com/file/xfk1u8yl4uqbizx/nulis.zip/file',
+        '12HqXBYK',
+        '67635Zxwjej',
+        'reply',
+        '664WLkovm',
+        'div.dl-info',
+        '\x0a📄\x20*Tipo:*\x20',
+        'data-scrambled-url',
+        'https://api.nekorinn.my.id/tools/rynn-stuff-v2',
+        'lookup',
+        'mediafire\x20<enlace>',
+        'RsPXK',
+        'rGDBp',
+        'message',
+        '1ZhsBMr',
+        '1028498rwtBYc',
+        'GET',
+        'error',
+        'mfdl',
+        'from',
+        '2590124jXoNog',
+        'find',
+        'chat',
+        'get',
+        '❌\x20No\x20se\x20pudo\x20obtener\x20el\x20enlace\x20de\x20descarga.',
+        'trim',
+        'ltOzZ',
+        'ul.details\x20li:nth-child(2)\x20span',
+        'tags',
+        'sendMessage',
+        'includes',
+        'a#downloadButton',
+        'toLowerCase',
+        '1467360kJwCpR',
+        '❌\x20El\x20enlace\x20no\x20es\x20válido.\x20Solo\x20se\x20aceptan\x20links\x20de\x20*mediafire.com*',
+        'uznGR',
+        'attr',
+        'toString',
+        'KTdwv',
+        '2003295ppmohd',
+        'bcTZb',
+        'application/octet-stream',
+        '44GVFBpW',
+        'help'
+    ];
+    _0xb206 = function () {
+        return _0x5d1fc7;
+    };
+    return _0xb206();
 }
-}
-
-    if (pesoMB <= 100) {
-      await conn.sendMessage(m.chat, {
-        document: { url: urlDescarga},
-        fileName: filename,
-        mimetype,
-        caption: mensaje,
-        contextInfo
-}, { quoted: m})
-} else {
-      await conn.sendMessage(m.chat, {
-        text: mensaje,
-        contextInfo
-}, { quoted: m})
-}
-
-} catch (error) {
-    console.error(error)
-    m.reply(`❌ Error al procesar el enlace: ${error.message}`)
-}
-}
-
-handler.command = ['mediafire', 'mf', 'mfdl']
-handler.help = ['mediafire <enlace>']
-handler.tags = ['descargas']
-
-export default handler
+import _0x452133 from 'axios';
+import * as _0x162be0 from 'cheerio';
+import _0x1c682f from 'mime-types';
+let handler = async (_0x1c5b0d, {
+    conn: _0x21aa65,
+    text: _0x3344b4
+}) => {
+    const _0x2bc6f1 = _0x68be, _0x2a6859 = {
+            'RsPXK': _0x2bc6f1(0xa3),
+            'KTdwv': _0x2bc6f1(0xab),
+            'ltOzZ': _0x2bc6f1(0x94),
+            'rGDBp': _0x2bc6f1(0xa0),
+            'IouLL': function (_0x5acf1c, _0x211872) {
+                return _0x5acf1c(_0x211872);
+            },
+            'bcTZb': _0x2bc6f1(0x96),
+            'neIHB': 'title',
+            'GjevD': _0x2bc6f1(0x98),
+            'bbhco': _0x2bc6f1(0xb6),
+            'YFHAw': function (_0x2d8f11, _0x2f31f2) {
+                return _0x2d8f11(_0x2f31f2);
+            },
+            'uznGR': _0x2bc6f1(0xa2),
+            'FbKdI': 'base64',
+            'eyJHL': function (_0x3e9ab3, _0x9dc44f) {
+                return _0x3e9ab3 * _0x9dc44f;
+            },
+            'MToIG': 'https://telegra.ph/file/7e6802b6c40fcf8fc4aa5.jpg'
+        };
+    if (!_0x3344b4)
+        return _0x1c5b0d[_0x2bc6f1(0x9e)](_0x2bc6f1(0x9b));
+    if (!/mediafire\.com/[_0x2bc6f1(0x8b)](_0x3344b4))
+        return _0x1c5b0d[_0x2bc6f1(0x9e)](_0x2bc6f1(0x7c));
+    try {
+        const {data: _0x25ebe3} = await _0x452133[_0x2bc6f1(0xb2)](_0x2a6859[_0x2bc6f1(0xa6)], {
+                'params': {
+                    'method': _0x2a6859[_0x2bc6f1(0x80)],
+                    'url': _0x3344b4,
+                    'accessKey': _0x2a6859[_0x2bc6f1(0xb5)]
+                }
+            }), _0x42c055 = _0x162be0['load'](_0x25ebe3['result']['response']), _0x185715 = _0x42c055(_0x2a6859[_0x2bc6f1(0xa7)]), _0x447cb0 = _0x2a6859['IouLL'](_0x42c055, _0x2a6859[_0x2bc6f1(0x82)])[_0x2bc6f1(0x7e)](_0x2a6859['neIHB']) || _0x185715[_0x2bc6f1(0xb0)](_0x2bc6f1(0x8e))[_0x2bc6f1(0x90)]()[_0x2bc6f1(0xb4)](), _0x501f9b = _0x447cb0['split']('.')[_0x2bc6f1(0x8f)](), _0x9aee43 = _0x1c682f[_0x2bc6f1(0xa4)](_0x501f9b[_0x2bc6f1(0xbb)]()) || _0x2bc6f1(0x83), _0x57577f = _0x185715[_0x2bc6f1(0xb0)](_0x2a6859[_0x2bc6f1(0x88)])[_0x2bc6f1(0x90)]()[_0x2bc6f1(0xb4)](), _0x45d119 = _0x185715['find'](_0x2a6859[_0x2bc6f1(0x87)])[_0x2bc6f1(0x90)]()[_0x2bc6f1(0xb4)](), _0x2cf7a1 = _0x2a6859[_0x2bc6f1(0x93)](_0x42c055, _0x2bc6f1(0xba))[_0x2bc6f1(0x7e)](_0x2a6859[_0x2bc6f1(0x7d)]);
+        if (!_0x2cf7a1)
+            return _0x1c5b0d['reply'](_0x2bc6f1(0xb3));
+        const _0x155e30 = Buffer[_0x2bc6f1(0xae)](_0x2cf7a1, _0x2a6859[_0x2bc6f1(0x91)])[_0x2bc6f1(0x7f)](), _0x54edb1 = _0x2a6859[_0x2bc6f1(0x89)](parseFloat(_0x57577f), _0x57577f[_0x2bc6f1(0xbb)]()[_0x2bc6f1(0xb9)]('gb') ? -0xf12 + -0x377 * -0x7 + 0x52f * -0x1 : -0x2029 * -0x1 + 0x1 * 0xd99 + -0x2dc1), _0x595104 = (_0x2bc6f1(0x97) + _0x447cb0 + _0x2bc6f1(0xa1) + _0x9aee43 + '\x0a📦\x20*Tamaño:*\x20' + _0x57577f + _0x2bc6f1(0x8a) + _0x45d119 + '\x0a')[_0x2bc6f1(0xb4)](), _0x39e695 = {
+                'externalAdReply': {
+                    'showAdAttribution': !![],
+                    'mediaUrl': _0x155e30,
+                    'mediaType': 0x1,
+                    'renderLargerThumbnail': !![],
+                    'thumbnailUrl': global['thumb'] || _0x2a6859[_0x2bc6f1(0x86)],
+                    'title': _0x447cb0,
+                    'body': _0x57577f + _0x2bc6f1(0x99) + _0x45d119,
+                    'sourceUrl': _0x155e30
+                }
+            };
+        _0x54edb1 <= -0x8 * -0x34 + 0x204 + -0x340 * 0x1 ? await _0x21aa65[_0x2bc6f1(0xb8)](_0x1c5b0d[_0x2bc6f1(0xb1)], {
+            'document': { 'url': _0x155e30 },
+            'fileName': _0x447cb0,
+            'mimetype': _0x9aee43,
+            'caption': _0x595104,
+            'contextInfo': _0x39e695
+        }, { 'quoted': _0x1c5b0d }) : await _0x21aa65[_0x2bc6f1(0xb8)](_0x1c5b0d[_0x2bc6f1(0xb1)], {
+            'text': _0x595104,
+            'contextInfo': _0x39e695
+        }, { 'quoted': _0x1c5b0d });
+    } catch (_0x320300) {
+        console[_0x2bc6f1(0xac)](_0x320300), _0x1c5b0d[_0x2bc6f1(0x9e)](_0x2bc6f1(0x9a) + _0x320300[_0x2bc6f1(0xa8)]);
+    }
+};
+handler[_0xd5aa92(0x8d)] = [
+    _0xd5aa92(0x8c),
+    'mf',
+    _0xd5aa92(0xad)
+], handler[_0xd5aa92(0x85)] = [_0xd5aa92(0xa5)], handler[_0xd5aa92(0xb7)] = ['descargas'];
+export default handler;
