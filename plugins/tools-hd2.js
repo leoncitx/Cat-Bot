c658cac4b1e2c7fa05e7841e3d9ea61b05daaf10
Wichi <sebastianbarbaro82@gmail.com>
2025-08-03 05:41:27 -0400

    Actualizar tools-hd2.js

--- plugins/tools-hd2.js
+++ plugins/tools-hd2.js
@@ -1,38 +1,39 @@
+
 import axios from 'axios'
 import FormData from 'form-data'
- 
-let handler = async (m, { conn }) => {
+
+let handler = async (m, { conn}) => {
   try {
-    const q = m.quoted ? m.quoted : m
+    const q = m.quoted? m.quoted: m
     const mime = (q.msg || q).mimetype || ''
-    
-    if (!mime) return m.reply('Reply to media (image/video/audio/document)')
-    
-    m.reply('*Uploading to MediaFire...*')
-    
+
+    if (!mime) return m.reply('Responde a un archivo multimedia (imagen/video/audio/documento)')
+
+    m.reply('*Subiendo a MediaFire...*')
+
     let media = await q.download()
     let form = new FormData()
     form.append('file', media, `fgsi_${Date.now()}.${mime.split('/')[1]}`)
-    
-    let { data } = await axios.post('https://fgsi.koyeb.app/api/upload/uploadMediaFire', form, {
+
+    let { data} = await axios.post('https://fgsi.koyeb.app/api/upload/uploadMediaFire', form, {
       headers: {
-        ...form.getHeaders(),
+...form.getHeaders(),
         'Content-Type': 'multipart/form-data',
         'apikey': 'fgsiapi-213d7253-6d'
-      }
-    })
-     
-    let result = data.data
-    
-    m.reply(`${result.links.normal_download}`)
-    
-  } catch (e) {
-    m.reply(e.message)
-  }
 }
- 
+})
+
+    let result = data.data
+
+    m.reply(`${result.links.normal_download}`)
+
+} catch (e) {
+    m.reply(`⚠️ Error: ${e.message}`)
+}
+}
+
 handler.help = ['upmf']
 handler.command = ['upmf']
-handler.tags = ['tools']
- 
+handler.tags = ['herramientas']
+
 export default handler
