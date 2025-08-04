f22de9abcbd67c58f3524bc46bf507c49fbaa43b
Wichi <sebastianbarbaro82@gmail.com>
2025-08-04 11:35:58 -0400

    Actualizar downloader-playmp4.js

--- plugins/downloader-playmp4.js
+++ plugins/downloader-playmp4.js
@@ -150,7 +150,6 @@ delete userRequests[m.sender];
 handler.help = ['play', 'play2', 'play3', 'play4', 'playdoc'];
 handler.tags = ['downloader'];
 handler.command = ['play', 'play2', 'play3', 'play4', 'audio', 'video', 'playdoc', 'playdoc2', 'musica'];
-handler.register = true;
 export default handler;
 
 async function search(query, options = {}) {
