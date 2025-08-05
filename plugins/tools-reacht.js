67a9cd9d13d47b31578bd35f66b82916f0fa6cd9
Wichi <sebastianbarbaro82@gmail.com>
2025-08-05 09:04:45 -0400

    Actualizar tools-reacht.js

--- plugins/tools-reacht.js
+++ plugins/tools-reacht.js
@@ -1,37 +1,24 @@
+import axios from 'axios'
 
-const handler = async (m, { conn }) => {
-    if (!args[0]) return m.reply(`ejemplo:\n.channelReact https://whatsapp.com/channel/xxxx hola`);
-
-if (!args[0].startsWith("https://whatsapp.com/channel/")) return m.reply("Link no es válido.");
-
-    const hurufGaya = {
-        a: '🅐', b: '🅑', c: '🅒', d: '🅓', e: '🅔', f: '🅕', g: '🅖',
-        h: '🅗', i: '🅘', j: '🅙', k: '🅚', l: '🅛', m: '🅜', n: '🅝',
-        o: '🅞', p: '🅟', q: '🅠', r: '🅡', s: '🅢', t: '🅣', u: '🅤',
-        v: '🅥', w: '🅦', x: '🅧', y: '🅨', z: '🅩',
-        '0': '⓿', '1': '➊', '2': '➋', '3': '➌', '4': '➍',
-        '5': '➎', '6': '➏', '7': '➐', '8': '➑', '9': '➒'
-    };
-
-    const emojiInput = args.slice(1).join(' ').toLowerCase();
-    const emoji = emojiInput.split('').map(c => {
-        if (c === ' ') return '―';
-        return hurufGaya[c] || c;
-    }).join('');
-
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    if (!text) return m.reply(`Contoh : .skiplink https://sub4unlock.co/S9oU0`);
+    m.reply('wett')
     try {
-        const link = args[0];
-        const channelId = link.split('/')[4];
-        const messageId = link.split('/')[5];
+        let api = `https://fgsi.koyeb.app/api/tools/skip/sub4unlock?apikey=APIKEY&url=${encodeURIComponent(text)}`;
+        let { data: json } = await axios.get(api);
 
-        const res = await conn.newsletterMetadata("invite", channelId);
-        await conn.newsletterReactMessage(res.id, messageId, emoji);
+        if (!json.status || !json.data?.linkGo) {
+            return m.reply('Lu masukin url apa tu woy 😂');
+        }
 
-        return m.reply(`Se envió correctamente la reacción *${emoji}* al mensaje en el canal *${res.name}*.`);
-    } catch (e) {
-        console.error(e);
-        return m.reply("Error al enviar reacción. Asegúrate de que el enlace y el emoji sean válidos.");
+        await m.reply(`${json.data.linkGo}`);
+    } catch (err) {
+        m.reply(`Eror kak : ${err.message}`)
     }
-}
-handler.command = /^(ch)$/i
+};
+
+handler.help = ['skiplinksub4unlock <url>'];
+handler.tags = ['tools'];
+handler.command = ['sklink', 'skiplinksub4unlock']
+
 export default handler
