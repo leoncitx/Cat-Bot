fe5062a6de4d72ef8aafc47cfdd8480d67b55f82
Wichi <sebastianbarbaro82@gmail.com>
2025-08-06 14:49:54 -0400

    Actualizar tools-clima.js

--- plugins/tools-clima.js
+++ plugins/tools-clima.js
@@ -1,63 +1,41 @@
-import axios from "axios";
-
-let handler = async (m, { conn, args }) => {
-  if (!args[0]) {
-    let resp = `*[❗INFO❗] ESCRIBA EL NOMBRE DE SU PAIS O CIUDAD*`;
-    let txt = '';
-    let count = 0;
-    for (const c of resp) {
-      await new Promise(resolve => setTimeout(resolve, 5));
-      txt += c;
-      count++;
-      if (count % 10 === 0) {
-        conn.sendPresenceUpdate('composing', m.chat);
-      }
-    }
-    await conn.sendMessage(m.chat, { text: txt.trim(), mentions: conn.parseMention(txt) }, { quoted: m, ephemeralExpiration: 24*60*60, disappearingMessagesInChat: 24*60*60 });
-    return;
-  }
-
-  try {
-    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${args[0]}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273`);
-    const res = response.data;
-    const name = res.name;
-    const Country = res.sys.country;
-    const Weather = res.weather[0].description;
-    const Temperature = res.main.temp + "°C";
-    const Minimum_Temperature = res.main.temp_min + "°C";
-    const Maximum_Temperature = res.main.temp_max + "°C";
-    const Humidity = res.main.humidity + "%";
-    const Wind = res.wind.speed + "km/h";
-    const wea = `「 📍 」LUGAR: ${name}\n「 🗺️ 」PAIS: ${Country}\n「 🌤️ 」TIEMPO: ${Weather}\n「 🌡️ 」TEMPERATURA: ${Temperature}\n「 💠 」TEMPERATURA MINIMA: ${Minimum_Temperature}\n「 📛 」TEMPERATURA MAXIMA: ${Maximum_Temperature}\n「 💦 」HUMEDAD: ${Humidity}\n「 🌬️ 」VIENTO: ${Wind}`.trim();
-
-    let txt = '';
-    let count = 0;
-    for (const c of wea) {
-      await new Promise(resolve => setTimeout(resolve, 5));
-      txt += c;
-      count++;
-      if (count % 10 === 0) {
-        conn.sendPresenceUpdate('composing', m.chat);
-      }
-    }
-    await conn.sendMessage(m.chat, { text: txt.trim(), mentions: conn.parseMention(txt) }, { quoted: m, ephemeralExpiration: 24*60*60, disappearingMessagesInChat: 24*60*60 });
-  } catch (e) {
-    let resp = " *[❗INFO❗] Error!\n _No se encontraron resultados, trate de escribir un país o ciudad existente._* ";
-    let txt = '';
-    let count = 0;
-    for (const c of resp) {
-      await new Promise(resolve => setTimeout(resolve, 5));
-      txt += c;
-      count++;
-      if (count % 10 === 0) {
-        conn.sendPresenceUpdate('composing', m.chat);
-      }
-    }
-    await conn.sendMessage(m.chat, { text: txt.trim(), mentions: conn.parseMention(txt) }, { quoted: m, ephemeralExpiration: 24*60*60, disappearingMessagesInChat: 24*60*60 });
-  }
+let handler = async (m, { conn, command }) => {
+if (!m.quoted) return m.reply(`《★》Responde a un mensaje para ${command === 'pin' ? 'fijarlo' : 'desfijarlo'}.`);
+try {
+let messageKey = {remoteJid: m.chat,
+fromMe: m.quoted.fromMe,
+id: m.quoted.id,
+participant: m.quoted.sender
 };
 
-handler.help = ['clima *<ciudad/país>*'];
-handler.tags = ['herramientas'];
-handler.command = /^(clima|tiempo)$/i;
-export default handler;
+if (command === 'pin' || command === 'fijar') {
+await conn.sendMessage(m.chat, { pin: messageKey,type: 1, time: 604800 })
+//conn.sendMessage(m.chat, {pin: {type: 1, time: 604800, key: messageKey }});
+m.react("✅️")
+}
+
+if (command === 'unpin' || command === 'desfijar') {
+await conn.sendMessage(m.chat, { pin: messageKey,type: 2, time: 86400 })
+//conn.sendMessage(m.chat, { pin: { type: 0, key: messageKey }});
+m.react("✅️")
+}
+
+if (command === 'destacar') {
+conn.sendMessage(m.chat, {keep: messageKey, type: 1, time: 15552000 })
+m.react("✅️")
+}
+
+if (command === 'desmarcar') {
+conn.sendMessage(m.chat, {keep: messageKey, type: 2, time: 86400 })
+m.react("✅️")
+}
+} catch (error) {
+console.error(error);
+}};
+handler.help = ['pin']
+handler.tags = ['grupo']
+handler.command = ['fijar', 'unpin', 'desfijar', 'destacar', 'desmarcar'] 
+handler.admin = true
+handler.group = true
+handler.botAdmin = true
+// handler.register = true 
+export default handler
